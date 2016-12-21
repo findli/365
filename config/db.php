9e800f0789033ec08d3046c68b732de4acea455f
Ya <mig291988@gmail.com>
2016-12-22 02:12:24 +0300

    add config file .env

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'       => 'yii\db\Connection',
+    'dsn'         => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_DATABASE') . ';port=' . getenv('DB_PORT'),
+    'username'    => getenv('DB_USERNAME'),
+    'password'    => getenv('DB_PASSWORD'),
+    'charset'     => 'utf8',
+    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
 ];
