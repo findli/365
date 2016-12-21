5d65ba57f7f338f97573c92c22089c62345941a4
Ya <mig291988@gmail.com>
2016-12-21 18:09:05 +0300

    init yii2

---
+++ tests/unit/models/UserTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace tests\models;
+use app\models\Category;
+
+class UserTest extends \Codeception\Test\Unit
+{
+    public function testFindUserById()
+    {
+        expect_that($user = Category::findIdentity(100));
+        expect($user->username)->equals('admin');
+
+        expect_not(Category::findIdentity(999));
+    }
+
+    public function testFindUserByAccessToken()
+    {
+        expect_that($user = Category::findIdentityByAccessToken('100-token'));
+        expect($user->username)->equals('admin');
+
+        expect_not(Category::findIdentityByAccessToken('non-existing'));
+    }
+
+    public function testFindUserByUsername()
+    {
+        expect_that($user = Category::findByUsername('admin'));
+        expect_not(Category::findByUsername('not-admin'));
+    }
+
+    /**
+     * @depends testFindUserByUsername
+     */
+    public function testValidateUser($user)
+    {
+        $user = Category::findByUsername('admin');
+        expect_that($user->validateAuthKey('test100key'));
+        expect_not($user->validateAuthKey('test102key'));
+
+        expect_that($user->validatePassword('admin'));
+        expect_not($user->validatePassword('123456'));        
+    }
+
+}
